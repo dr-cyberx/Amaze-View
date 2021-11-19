d8847228b6625da4e64d7abafddcdb1a3d589c0b
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-11-19 13:25:37 +0530

    changes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,22 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.scss";
+import AmazeLoader from "@components/reusable/Loader";
 
 const Home: NextPage = () => {
+  const [isUserVerified, setIsUserVerified] = useState(false);
   const router = useRouter();
 
+  const verifyUser = async () => {
+    const token = await localStorage.getItem("auth-Token");
+    return new Promise((resolve: any, reject: any) => {
+      if (token) {
+        resolve(true);
+      } else {
+        reject(false);
+      }
+    });
+  };
+
   useEffect(() => {
-    const token = localStorage.getItem("auth-Token");
-    if (token) {
-      router.push("/Home");
-    }
-    router.push("/auth/Login");
+    verifyUser()
+      .then((data) => {
+        setIsUserVerified(true);
+        router.push("/Home");
+      })
+      .catch((err) => {
+        setIsUserVerified(false);
+        router.push("/auth/Login");
+      });
   }, []);
 
-  return null;
+  return <AmazeLoader data={true} />;
 };
 
 export default Home;
